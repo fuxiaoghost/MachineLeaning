f665e73b2c34858d581843cc10705afd3163b312
Kirn <wangsguang@gmail.com>
2017-12-14 13:11:38 +0800

    kNN completed

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -4,9 +4,9 @@ import operator
 import matplotlib
 import matplotlib.pyplot as plt
 import matFont
+import os
 
-
-def createDataSet():
+def create_data_set():
     group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
     return group, labels
@@ -59,6 +59,26 @@ def file2matrix(filename):
             index += 1
         return returnMat, classLabelVector
 
+def drawScatter(datingDataMat):
+    matFont.set_matplot_zh_font()
+    fig = plt.figure('datingDataMat')
+    ax = fig.add_subplot(111)
+    ax.set_title(u'散点图')
+    ax.set_xlabel(u'玩视频游戏所耗时间百分比')
+    ax.set_ylabel(u'每周消费的冰激凌公升数')
+    ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2],
+           20.0 * array(datingLabels), 15.0 * array(datingLabels))
+    plt.show()
+
+
+def img2vector(filename):
+    returnVector = zeros((1, 1024))
+    with open(filename, 'r') as fr:
+        for i in range(32):
+            lineStr = fr.readline()
+            for j in range(32):
+                returnVector[0, 32 * i + j] = int(lineStr[j])
+        return returnVector
 
 def datingClassTest():
     hoRatio = 0.15
@@ -76,15 +96,35 @@ def datingClassTest():
             errorCount += 1.0
     print 'the total error rate is:%f' % (errorCount / float(numTestVecs))
 
+def handwritingClassTest():
+    # 加载训练数据
+    hwLabels = []
+    trainingFileList = os.listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr =  fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s' % fileNameStr)
+    # 加载测试数据
+    testFileList = os.listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr =  fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print '%d the classifier came back with:%d, the real answer is:%d' % (i, classifierResult, classNumStr)
+        if (classifierResult != classNumStr):
+            errorCount += 1.0
+    print 'the total error rate is:%f' % (errorCount / float(mTest))
 
-# matFont.set_matplot_zh_font()
-datingClassTest()
+# 约会人群分类
+# datingClassTest()
 
-
-# fig = plt.figure('datingDataMat')
-# ax = fig.add_subplot(111)
-# ax.set_title(u'散点图')
-# ax.set_xlabel(u'玩视频游戏所耗时间百分比')
-# ax.set_ylabel(u'每周消费的冰激凌公升数')
-# ax.scatter(datingDataMat[:,1], datingDataMat[:,2], 20.0 * array(datingLabels), 15.0 * array(datingLabels))
-# plt.show()
+# 手写识别
+handwritingClassTest()
